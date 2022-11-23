1b5f5e707f1e758c5be86231497682538e110949
FmHasibul <fmhhasan@gmail.com>
2022-11-23 21:47:47 +0600

    server created

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express()
 
+
+
+app.get('/', (req, res) => {
+    res.send('Book resell server is running')
+})
+
+
+app.listen(port, () => {
+    console.log(`Book Resell server is running in port ${port}`);
+})
