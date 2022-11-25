8ab7f3ea9e9b8d8b1dd155abd82ae2a0acf89705
FmHasibul <fmhhasan@gmail.com>
2022-11-25 23:46:45 +0600

    user collection created

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express()
 require('dotenv').config();
-const category = require('./category.json')
-const books = require('./books.json');
 
 
 
@@ -17,6 +15,29 @@ const uri = process.env.DB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
+        const categoryCollections = client.db('bookResell').collection('categorylist')
+        const productCollections = client.db('bookResell').collection('products')
+        const userCollections = client.db('bookResell').collection('users')
+
+        app.get('/category', async (req, res) => {
+            const query = {};
+            const options = await categoryCollections.find(query).toArray();
+
+            res.send(options)
+        });
+        app.get('/category/:id', async (req, res) => {
+            const id = parseInt(req.params.id)
+            const query = { categoryId: id }
+            console.log(query);
+            const matching = await productCollections.find(query).toArray()
+            res.send(matching)
+        });
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            const result = await userCollections.insertOne(user)
+            res.send(result)
+        })
+
 
     }
 
@@ -25,16 +46,11 @@ async function run() {
 }
 
 run().catch(err => console.log(err))
-app.get('/category', (req, res) => {
-    res.send(category)
-})
-app.get('/category/:id', (req, res) => {
-    const id = req.params.id
-    const matching = books.filter(ctg => ctg.categoryId == id)
 
 
-    res.send(matching)
-})
+
+
+
 
 
 
